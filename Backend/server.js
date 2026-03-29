fd0a0419bee3b71dedda6b9bf7bdd0bdac0ce86a
Ronak Mahajan <ronakmahajan123@gmail.com>
2026-03-29 18:00:01 +0530

    cors

--- Backend/server.js
+++ Backend/server.js
@@ -13,13 +13,9 @@ app.use(cors({
     "https://collegemanagement-erp.netlify.app",
     "http://localhost:5173"
   ],
-  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-
-
-app.options("/*", cors());
 app.use(express.json());
 
 // Routes
